a1f4941ccc79480818cb6b9b3d01787edb966058
Michael Cruz <m.cruz@quarterspot.com>
2019-03-12 15:14:45 -0500

    Azure and ps

---
+++ envvar.psm1
@@ -0,0 +1,39 @@
+function set-envvar{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [validateset("AWS","Azure")]
+        [string]$provider,
+        [Parameter(Mandatory)]
+        [pscredential]$Credential,
+        [string]$KeyVault = "kv-cruz3r",
+        [string]$subscriptionname = 'Visual Studio Premium with MSDN'
+    )
+
+    if ($provider -eq "AWS"){
+        Write-Output "AWS Env Variables"
+        Login-AzureRmAccount -Credential $Credential | out-null
+        Get-AzureRmSubscription -SubscriptionName $subscriptionname| Set-AzureRmContext | Out-Null
+        $env:AWS_ACCESS_KEY_ID = (Get-AzureKeyVaultSecret  -vaultName $KeyVault -Name "awsAccessKeyId" ).SecretValueText
+        $env:AWS_SECRET_ACCESS_KEY = (Get-AzureKeyVaultSecret  -vaultName $KeyVault -Name "awsSecretAccessKey" ).SecretValueText
+    }elseif ($provider -eq "Azure"){
+        Write-Output "Azure Env Variables"
+        Login-AzureRmAccount -Credential $Credential | out-null
+        Get-AzureRmSubscription -SubscriptionName $subscriptionname| Set-AzureRmContext | Out-Null
+        $env:ARM_SUBSCRIPTION_ID = (Get-AzureKeyVaultSecret  -vaultName $KeyVault -Name "azuresecretid" ).SecretValueText
+        $env:ARM_CLIENT_ID = (Get-AzureKeyVaultSecret  -vaultName $KeyVault -Name "azureclientid" ).SecretValueText
+        $env:ARM_CLIENT_SECRET = (Get-AzureKeyVaultSecret  -vaultName $KeyVault -Name "asureclientsecret" ).SecretValueText
+        $env:ARM_TENANT_ID = (Get-AzureKeyVaultSecret  -vaultName $KeyVault -Name "azuretenantid" ).SecretValueText
+    }
+}
+
+function remove-envvar {
+    param()
+    Write-Output "Removing Azure and AWS Variables"
+    Remove-Item env:\AWS_ACCESS_KEY_ID -ea SilentlyContinue
+    Remove-Item env:\AWS_SECRET_ACCESS_KEY -ea SilentlyContinue
+    Remove-Item env:\ARM_SUBSCRIPTION_ID -ea SilentlyContinue
+    Remove-Item env:\ARM_CLIENT_ID -ea SilentlyContinue
+    Remove-Item env:\ARM_CLIENT_SECRET -ea SilentlyContinue
+    Remove-Item env:\ARM_TENANT_ID -ea SilentlyContinue
+}
